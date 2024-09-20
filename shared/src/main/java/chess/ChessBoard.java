8ee315262e236c96bfa85f41dac8715b0ffbefb0
yuranb <yuhaoz0120@gmail.com>
2024-09-20 06:28:37 -0600

    Passed two tests of bishop&rook

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -13,7 +13,6 @@ public class ChessBoard {
     private ChessPiece[][] squares = new ChessPiece[8][8];
 
     public ChessBoard() {
-        resetBoard();
     }
 
     /**
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
-
+import java.util.List;
 /**
  * Represents a single chess piece
  * <p>
@@ -67,6 +67,121 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        ChessPiece currentPiece = board.getPiece(myPosition);
+        PieceType currentType = currentPiece.pieceType;
+        List<ChessMove> moves = new ArrayList<>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        if (currentType == PieceType.BISHOP) {
+            moves = diagonal(board, row, col, color, currentType);
+        } else if (currentType == PieceType.ROOK) {
+            moves = straight(board, row, col, color, currentType);
+        } /*else if (currentType == PieceType.QUEEN) {
+            moves.addAll(diagonal(board, row, col, color, currentType));
+            moves.addAll(straight(board, row, col, color, currentType));
+        } else if (currentType == PieceType.KING) {
+            moves.addAll(diagonal(board, row, col, color, currentType));
+            moves.addAll(straight(board, row, col, color, currentType));
+        } else if (currentType == PieceType.KNIGHT) {
+            moves = knightMove(board, row, col, color, currentType);
+        } else if (currentType == PieceType.PAWN) {
+            moves = pawnMoves(board, row, col, color, currentType);
+        } else {
+            throw new IllegalArgumentException("Wrong piece_type: " + currentType);
+        }*/
+        return moves;
     }
-}
+
+    public static List<ChessMove> diagonal(ChessBoard chessBoard, int row, int col, ChessGame.TeamColor currentColor, PieceType currentType) {
+        ChessPosition s_Position = new ChessPosition(row, col);
+        List<ChessMove> valid_moves = new ArrayList<>();
+
+        int[][] directions = {
+                {1, 1},    // right up
+                {1, -1},   // right down
+                {-1, 1},   // left up
+                {-1, -1}   // left down
+        };
+
+        for (int[] direction : directions) {
+            int newRow = row;
+            int newCol = col;
+
+            // Move in the specified diagonal direction
+            while (true) {
+                newRow += direction[0];
+                newCol += direction[1];
+
+                // Break the loop if out of bounds
+                if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) {
+                    break;
+                }
+
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece pieceAtPos = chessBoard.getPiece(newPos);
+
+                // Stop if there is a piece of the same color or capture if opponent's piece
+                if (pieceAtPos != null) {
+                    if (pieceAtPos.getTeamColor() == currentColor) {
+                        break;  // Stop if it's our own piece
+                    } else {
+                        valid_moves.add(new ChessMove(s_Position, newPos, null));  // Capture opponent's piece
+                        break;  // And then stop
+                    }
+                }
+
+                // Add the move as it's valid and continue
+                valid_moves.add(new ChessMove(s_Position, newPos, null));
+
+                if (currentType == PieceType.KING) {
+                    break;
+                }
+            }
+        }
+
+        return valid_moves;
+    }
+    public static List<ChessMove> straight(ChessBoard chessBoard, int row, int col, ChessGame.TeamColor currentColor, PieceType currentType) {
+        ChessPosition s_Position = new ChessPosition(row, col);
+        List<ChessMove> valid_moves = new ArrayList<>();
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};  // up, down, right, left
+
+        for (int[] direction : directions) {
+            int newRow = row;
+            int newCol = col;
+
+            while (true) {
+                newRow += direction[0];
+                newCol += direction[1];
+
+                // Break the loop if out of bounds
+                if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) {
+                    break;
+                }
+
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece pieceAtPos = chessBoard.getPiece(newPos);
+
+                // Stop if there is a piece of the same color or capture if opponent's piece
+                if (pieceAtPos != null) {
+                    if (pieceAtPos.getTeamColor() == currentColor) {
+                        break;  // Stop if it's our own piece
+                    } else {
+                        valid_moves.add(new ChessMove(s_Position, newPos, null));  // Capture opponent's piece
+                        break;  // And then stop
+                    }
+                }
+
+                // Add the move as it's valid and continue
+                valid_moves.add(new ChessMove(s_Position, newPos, null));
+
+                if (currentType == PieceType.KING) {
+                    break;
+                }
+            }
+        }
+
+        return valid_moves;
+    }
+    }
