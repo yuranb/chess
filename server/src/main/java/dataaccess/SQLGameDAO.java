678c2118aef20f732b1c10ff633f6d57f44bfe81
yuranb <yuhaoz0120@gmail.com>
2024-10-30 01:30:06 -0600

    Added the SQLGameDAO, Passed DatabaseTests but got 2 error from StandardAPITests

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,33 +1,132 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.*;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLGameDAO implements GameDAO{
-    @Override
-    public void createGame(GameData game) throws DataAccessException {
+    public SQLGameDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
+    @Override
+    public void createGame(GameData gameData) throws DataAccessException {
+        String statement = "INSERT INTO game (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?, ?, ?)";
+        String gameJson = new Gson().toJson(gameData.game());
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setInt(1, gameData.gameID());
+            ps.setString(2, gameData.whiteUsername());
+            ps.setString(3, gameData.blackUsername());
+            ps.setString(4, gameData.gameName());
+            ps.setString(5, gameJson);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error creating game: " + e.getMessage());
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        String statement = "SELECT * FROM game WHERE gameID = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setInt(1, gameID);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String gameName = rs.getString("gameName");
+                    String whiteUsername = rs.getString("whiteUsername");
+                    String blackUsername = rs.getString("blackUsername");
+                    String gameJson = rs.getString("chessGame");
+                    ChessGame chessGame = new Gson().fromJson(gameJson, ChessGame.class);
+                    return new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame);
+                } else {
+                    throw new DataAccessException("Game not found with ID: " + gameID);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error retrieving game: " + e.getMessage());
+        }
     }
 
     @Override
     public List<GameData> listGames() throws DataAccessException {
-        return List.of();
+        List<GameData> games = new ArrayList<>();
+        String statement = "SELECT * FROM game";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                int gameID = rs.getInt("gameID");
+                String gameName = rs.getString("gameName");
+                String whiteUsername = rs.getString("whiteUsername");
+                String blackUsername = rs.getString("blackUsername");
+                String gameJson = rs.getString("chessGame");
+                ChessGame chessGame = new Gson().fromJson(gameJson, ChessGame.class);
+                games.add(new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame));
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error listing games: " + e.getMessage());
+        }
+        return games;
     }
 
     @Override
-    public void updateGame(GameData game) throws DataAccessException {
+    public void updateGame(GameData gameData) throws DataAccessException {
+        String statement = "UPDATE game SET whiteUsername = ?, blackUsername = ?, chessGame = ? WHERE gameID = ?";
+        String gameJson = new Gson().toJson(gameData.game());
 
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.setString(1, gameData.whiteUsername());
+            ps.setString(2, gameData.blackUsername());
+            ps.setString(3, gameJson);
+            ps.setInt(4, gameData.gameID());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error updating game: " + e.getMessage());
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String statement = "TRUNCATE game";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error clearing games: " + e.getMessage());
+        }
+    }
 
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS game (
+                gameID int NOT NULL,
+                whiteUsername varchar(256),
+                blackUsername varchar(256),
+                gameName varchar(256),
+                chessGame TEXT,
+                PRIMARY KEY (gameID)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
 
